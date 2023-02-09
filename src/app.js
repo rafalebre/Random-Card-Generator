9dc95dc25fa40bc45a5710aead83d9deb338f3a0
rafalebre <rafaellebre100@gmail.com>
2023-02-09 20:28:06 +0000

    First

--- src/app.js
+++ src/app.js
@@ -6,6 +6,28 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  document.querySelector("#testing").innerHTML = cardGenerator();
+};
+let cardGenerator = function() {
+  let cardNumber = [
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+  let cardNumberSelect = Math.floor(Math.random() * cardNumber.length);
+  let cardSuits = ["♦", "♥", "♠", "♣"];
+
+  let cardSuitsSelect = Math.floor(Math.random() * cardSuits.length);
+
+  return cardNumber[cardNumberSelect] + cardSuits[cardSuitsSelect];
 };
